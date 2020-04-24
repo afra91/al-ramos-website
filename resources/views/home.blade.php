f42bb8c22664615a042df007852371b4ced28735
Alvaro Ramos <afra91@hotmail.com>
2020-04-23 22:57:40 -0500

    Boilerplate sass files, JetBrainsMono font and main background image added.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href={{ mix('css/app.css') }} rel="stylesheet">
+    </head>
+    <body>
+        <main role="main">
+
+        </main>
+        @if(config('app.env') == 'local')
+            <script src="http://localhost:35729/livereload.js"></script>
+        @endif 
+        <script src={{ mix('js/app.js') }}></script>
+    </body>
+</html>
